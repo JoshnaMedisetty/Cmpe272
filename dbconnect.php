7acfd4ff0c760fa78fcf5ddb94fdf477ef5e82d6
JoshnaMedisetty <107022821+JoshnaMedisetty@users.noreply.github.com>
2023-10-31 20:09:02 -0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -11,13 +11,13 @@ CREATE TABLE products(id INTEGER, name varchar(255), desc TEXT, hits INTEGER);
 
 
 
-INSERT INTO products VALUES(1,"yoga","Lorem ipsum dolor sit amet, consectetur adipiscing",0);
-INSERT INTO products VALUES(2,"areobic","Lorem ipsum dolor sit amet, consectetur adipiscing",0);
-INSERT INTO products VALUES(3,"cardio1","Lorem ipsum dolor sit amet, consectetur adipiscing",0);
-INSERT INTO products VALUES(4,"cardio2","Lorem ipsum dolor sit amet, consectetur adipiscing",0);
-INSERT INTO products VALUES(5,"cardio3","Lorem ipsum dolor sit amet, consectetur adipiscing",0);
-INSERT INTO products VALUES(6,"cardio4","Lorem ipsum dolor sit amet, consectetur adipiscing",0);
-INSERT INTO products VALUES(7,"cardio5","Lorem ipsum dolor sit amet, consectetur adipiscing",0);
-INSERT INTO products VALUES(8,"cardio6","Lorem ipsum dolor sit amet, consectetur adipiscing",0);
-INSERT INTO products VALUES(9,"cardio7","Lorem ipsum dolor sit amet, consectetur adipiscing",0);
-INSERT INTO products VALUES(10,"cardio8","Lorem ipsum dolor sit amet, consectetur adipiscing",0);
+INSERT INTO products VALUES(1,"Currency Wallet","With CryptoGuard managing your cryptocurrency portfolio has never been easier. Our multi-currency wallet allows you to securely store, send, and receive a wide range of cryptocurrencies, all within one convenient platform. Enjoy real-time balance tracking and easy transaction management",0);
+INSERT INTO products VALUES(2,"Security Storage","At CryptoGuard security is our top priority. We employ state-of-the-art encryption protocols and cold storage solutions to safeguard your digital assets. Our advanced security measures ensure that your cryptocurrencies remain protected against any potential threats. ",0);
+INSERT INTO products VALUES(3,"Expert Support","Have a question or need assistance? Our team of dedicated crypto experts is available around the clock to provide you with timely and knowledgeable support. Whether you're a newcomer or a seasoned trader, our experts are here to guide you through any aspect of the crypto world.",0);
+INSERT INTO products VALUES(4,"Secure Transactions","Our platform ensures the highest level of security for your transactions, providing you with peace of mind.",0);
+INSERT INTO products VALUES(5,"Portfolio Diversification","Diversify your investments across a wide range of cryptocurrencies to minimize risk and maximize potential returns. ",0);
+INSERT INTO products VALUES(6,"Real-time Market Insights","Stay informed with real-time market data trends  and insights to make informed decisions about your investments. ",0);
+INSERT INTO products VALUES(7,"User-Friendly Interface","Our intuitive interface makes it easy for both beginners and experienced users to navigate and manage their portfolios",0);
+INSERT INTO products VALUES(8,"Multi-Currency Wallet","re, send, and receive a wide range of cryptocurrencies securely within a single convenient platform.",0);
+INSERT INTO products VALUES(9,"Automated Trading","Take advantage of automated trading strategies to optimize your investment performance without constant manual monitoring.",0);
+INSERT INTO products VALUES(10,"Advanced Analytics","Access advanced analytics tools to gain deeper insights into your portfolio's performance and make data-driven decisions.",0);
