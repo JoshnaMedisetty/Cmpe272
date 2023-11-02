1c57020a78811db6586c0261e08d310159034903
JoshnaMedisetty <107022821+JoshnaMedisetty@users.noreply.github.com>
2023-11-01 18:57:39 -0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,18 +1,6 @@
 <?php
 
-// connection string
-$connectionString = "mongodb+srv://joshnamedisetty:Mongodb-123@cluster0.8y8zddn.mongodb.net/?retryWrites=true&w=majority";
-
-//  establish a connection
-try {
-    $client = new MongoDB\Client($connectionString);
-
-    // Select the database
-    $db = $client->selectDatabase('Cluster0');
-} catch (MongoDB\Driver\Exception\Exception $e) {
-    // Handle connection error
-    echo "Error connecting to MongoDB: " . $e->getMessage();
-}
+$mongoConnection = new MongoDB\Driver\Manager("mongodb+srv://joshnamedisetty:Mongodb-123@cluster0.8y8zddn.mongodb.net/Cluster0?retryWrites=true&w=majority");
 
 ?>
 
