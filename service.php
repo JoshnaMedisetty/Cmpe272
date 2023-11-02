7ba58a1d6500216ba8d85d4ce17243b3335bc681
JoshnaMedisetty <107022821+JoshnaMedisetty@users.noreply.github.com>
2023-11-01 18:40:28 -0700

    Update service.php

--- service.php
+++ service.php
@@ -115,7 +115,7 @@
               <div class="img-box">
                 <img src="images/s1.png" alt="">
               </div>
-              <a href="s1.php">
+              <a href="./s1.php">
               <div class="detail-box">
                 <h5>
                   Currency Wallet
