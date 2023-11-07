f3a561622d287d82740e6546b27a5642e31c5dff
JoshnaMedisetty <107022821+JoshnaMedisetty@users.noreply.github.com>
2023-11-07 15:20:12 -0800

    Update s1.php

--- s1.php
+++ s1.php
@@ -1,21 +1,21 @@
 <?php require "dbconnect.php"?>
 <?php
     // parse_str($_SERVER['QUERY_STRING']);
-    $result = $mysqli->query("SELECT * FROM products where id = 10;");
+    $result = $mysqli->query("SELECT * FROM products where id = 1;");
     $prod = $result -> fetch_assoc();
     $hits = $prod["hits"] + 1;
-    $mysqli->query("UPDATE products SET hits = ".$hits." WHERE id = 10;");    
+    $mysqli->query("UPDATE products SET hits = ".$hits." WHERE id = 1;");    
     $mysqli->close();
 ?>
 <?php
     if(isset($_COOKIE["lastids"])){
         if(explode(",",$_COOKIE["lastids"])[0]!=$prod["id"]){
-            setcookie("lastids",'10'.",".$_COOKIE["lastids"],time() + (86400 * 30));    
+            setcookie("lastids",'1'.",".$_COOKIE["lastids"],time() + (86400 * 30));    
         }
         
     }
     else{
-        setcookie("lastids", '10', time() + (86400 * 30));
+        setcookie("lastids", '1', time() + (86400 * 30));
     }
 ?>
 
