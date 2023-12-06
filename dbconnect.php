967af5f6e11ebf4c7a710972a7a0e9f143bf3011
JoshnaMedisetty <107022821+JoshnaMedisetty@users.noreply.github.com>
2023-12-06 10:18:02 -0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-$username = "sql3663844";
-$password = "Q7PmRCPdHS";
-$database = "sql3663844";
-$mysqli = new mysqli("sql3.freesqldatabase.com", $username, $password, $database, 3306);
+$username = "sql5668278";
+$password = "xFzjp2Yspa";
+$database = "sql5668278";
+$mysqli = new mysqli("sql5.freesqldatabase.com", $username, $password, $database, 3306);
 
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
